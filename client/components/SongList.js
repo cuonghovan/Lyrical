b923f69a9c9a5b15724338c9ab0c7552f31e702a
Bepatient <bepatient@framgia0208-pc.framgia.com>
2018-05-23 10:58:17 +0700

    Delete songs

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router-dom';
+import gql from 'graphql-tag';
 import fetchSongsQuery from '../queries/fetchSongs';
+
 
 class SongList extends React.Component {
   
+  onSongDelete(id) {
+    this.props.mutate({
+      variables: {id}      
+    })
+    .then(() => this.props.data.refetch());
+  }
+
   renderSongs() {
     return this.props.data.songs.map(song => (
-      <li className='collection-item' key={song.id}>{song.title}</li>
+      <li className='collection-item' key={song.id}>
+        {song.title}
+        <i className='material-icons' onClick={() => this.onSongDelete(song.id)}>delete</i>
+      </li>
     ));
   }
 
@@ -29,4 +41,14 @@ class SongList extends React.Component {
   }
 }
 
-export default graphql(fetchSongsQuery)(SongList);
+const mutation = gql`
+  mutation DeleteSong($id: ID) {
+    deleteSong(id: $id) {
+      id
+    }
+  }
+`;
+
+export default graphql(mutation)(
+  graphql(fetchSongsQuery)(SongList)
+);
